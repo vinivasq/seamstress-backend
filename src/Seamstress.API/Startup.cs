acb8a6800040cfa7a69b6eb553bc8a24b5f2690a
Vinicius <vinicius.vasques@josapar.com.br>
2023-10-10 14:41:13 -0300

    db connection string

--- src/Seamstress.API/Startup.cs
+++ src/Seamstress.API/Startup.cs
@@ -21,7 +21,7 @@ namespace Seamstress.API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddDbContext<SeamstressContext>(
-        context => context.UseNpgsql("Username=postgres;Password=12345;Database=seamstress;Port=5432;Host=localhost"));
+        context => context.UseNpgsql("host=containers-us-west-43.railway.app;port=7916;username=postgres;password=Ip9wIRrd9a3Bk5kOPaGD;database=railway"));
 
       services.AddControllers().AddJsonOptions(options =>
       {
