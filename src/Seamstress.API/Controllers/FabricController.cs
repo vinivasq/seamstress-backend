e59e4f459907b8232a8727fba7eaca5736d9307e
vinivasq <vinivasq@hotmail.com>
2023-10-24 22:18:06 -0300

    added authorize to remaining controllers

--- src/Seamstress.API/Controllers/FabricController.cs
+++ src/Seamstress.API/Controllers/FabricController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Seamstress.Application.Contracts;
 using Seamstress.Domain;
 
 namespace Seamstress.API.Controllers
 {
+  [Authorize]
   [ApiController]
   [Route("api/[controller]")]
   public class FabricController : ControllerBase
