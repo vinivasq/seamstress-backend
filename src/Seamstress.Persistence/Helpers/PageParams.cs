99b016a6a554215a0fc4eb169fbeb3167c754018
vinivasq <vinivasq@hotmail.com>
2024-01-04 20:57:18 -0300

    Changed term to initiate as empy string instead of null to prevent exceptions

--- src/Seamstress.Persistence/Helpers/PageParams.cs
+++ src/Seamstress.Persistence/Helpers/PageParams.cs
@@ -10,7 +10,7 @@ namespace Seamstress.Persistence.Helpers
       get { return pageSize; }
       set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
     }
-    public string Term { get; set; } = null!;
+    public string Term { get; set; } = string.Empty;
 
   }
 }
