49bcd1ab1fbafda1e3bea2dd7764026a00860a1e
vinivasq <vinivasq@hotmail.com>
2024-02-25 16:00:17 -0300

    Closes #24; Added UF to Customer

---
+++ src/Seamstress.Persistence/Migrations/20240225152257_AddedUFToCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seamstress.Persistence.Migrations
+{
+    public partial class AddedUFToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UF",
+                table: "Customers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UF",
+                table: "Customers");
+        }
+    }
+}
