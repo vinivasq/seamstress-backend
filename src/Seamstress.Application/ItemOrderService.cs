0d7e6240a8c19f2390b01a105f26b734b2151d20
vinivasq <vinivasq@hotmail.com>
2024-04-15 18:15:31 -0300

    uptade item order route setting new order total when updating or deleting a itemOrder with transaction control

--- src/Seamstress.Application/ItemOrderService.cs
+++ src/Seamstress.Application/ItemOrderService.cs
@@ -11,17 +11,20 @@ namespace Seamstress.Application
     private readonly IGeneralPersistence _generalPersistence;
     private readonly IItemOrderPersistence _itemOrderPersistence;
     private readonly IItemPersistence _itemPersistence;
+    private readonly IOrderPersistence _orderPersistence;
     private readonly IMapper _mapper;
 
 
     public ItemOrderService(IGeneralPersistence generalPersistence,
                             IItemOrderPersistence itemOrderPersistence,
                             IItemPersistence itemPersistence,
+                            IOrderPersistence orderPersistence,
                             IMapper mapper)
     {
       this._generalPersistence = generalPersistence;
       this._itemOrderPersistence = itemOrderPersistence;
       this._itemPersistence = itemPersistence;
+      this._orderPersistence = orderPersistence;
       this._mapper = mapper;
 
     }
@@ -53,20 +56,90 @@ namespace Seamstress.Application
       }
     }
 
+    public async Task<ItemOrder> UpdateItemOrder(int id, ItemOrderInputDto model)
+    {
+      try
+      {
+        await ValidateItemOrder(model);
+        ItemOrder itemOrder = await this._itemOrderPersistence.GetItemOrderByIdAsync(id)
+          ?? throw new Exception("Não foi possível recuperar o item de pedido a ser alterado");
+
+        _generalPersistence.BeginTransaction();
+
+        _mapper.Map(model, itemOrder);
+        _generalPersistence.Update(itemOrder);
+
+        if (await _generalPersistence.SaveChangesAsync())
+        {
+          ItemOrder itemOrderResponse = await this._itemOrderPersistence.GetItemOrderByIdAsync(itemOrder.Id)
+            ?? throw new Exception("Não foi possível recuperar o item de pedido após alteração");
+
+          Order order = await this._orderPersistence.GetOrderByIdAsync(itemOrderResponse.OrderId)
+            ?? throw new Exception("Não foi possível recuperar o  pedido a ser alterado");
+
+          decimal total = 0;
+          foreach (var item in order.ItemOrders)
+          {
+            total += item.Item!.Price * item.Amount;
+          }
+
+          order.Total = total;
+
+          this._generalPersistence.Update(order);
+
+          if (this._generalPersistence.SaveChanges() == false)
+            throw new Exception("Erro ao atualizar o total do pedido");
+
+          this._generalPersistence.CommitTransaction();
+          return itemOrderResponse;
+        }
+
+        throw new Exception("Não foi possível salvar as alterações no item de pedido");
+      }
+      catch (Exception ex)
+      {
+        this._generalPersistence.RollbackTransaction();
+        throw new Exception(ex.Message);
+      }
+    }
+
     public async Task<bool> DeleteItemOrder(int id)
     {
       try
       {
-        ItemOrder itemOrder = await _itemOrderPersistence.GetItemOrderByIdAsync(id) ??
-        throw new Exception("Não foi possível localizar o item a ser deletado");
+        ItemOrder itemOrder = await _itemOrderPersistence.GetItemOrderByIdAsync(id)
+          ?? throw new Exception("Não foi possível localizar o item a ser deletado");
 
+        _generalPersistence.BeginTransaction();
         _generalPersistence.Delete(itemOrder);
 
-        return await _generalPersistence.SaveChangesAsync();
+        if (await _generalPersistence.SaveChangesAsync())
+        {
+          Order order = await this._orderPersistence.GetOrderByIdAsync(itemOrder.OrderId)
+            ?? throw new Exception("Não foi possível recuperar o  pedido a ser alterado");
+
+          decimal total = 0;
+          foreach (var item in order.ItemOrders)
+          {
+            total += item.Item!.Price * item.Amount;
+          }
+
+          order.Total = total;
+
+          this._generalPersistence.Update(order);
+
+          if (this._generalPersistence.SaveChanges() == false)
+            throw new Exception("Erro ao atualizar o total do pedido");
+
+          this._generalPersistence.CommitTransaction();
+          return true;
+        };
+
+        throw new Exception("Não foi possível salvar as alterações no item de pedido");
       }
       catch (Exception ex)
       {
-
+        this._generalPersistence.RollbackTransaction();
         throw new Exception(ex.Message);
       }
     }
