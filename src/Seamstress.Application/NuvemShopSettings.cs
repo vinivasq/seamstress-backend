153ffa8c237d0136d76e24af292c65f2d7653344
vinicius.vasques <vinicius.vasques@harbor.com.br>
2026-03-23 19:51:29 -0300

    feat: add NuvemShopSettings config POCO and appsettings section

---
+++ src/Seamstress.Application/NuvemShopSettings.cs
@@ -0,0 +1,9 @@
+namespace Seamstress.Application
+{
+    public class NuvemShopSettings
+    {
+        public string AccessToken { get; set; } = null!;
+        public string UserAgent { get; set; } = null!;
+        public string StoreId { get; set; } = null!;
+    }
+}
